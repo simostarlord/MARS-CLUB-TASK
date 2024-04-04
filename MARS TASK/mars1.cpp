e692dadcf57e05c46bfc43f3426c164a13d131c5
simostarlord <157518754+simostarlord@users.noreply.github.com>
2024-04-04 18:31:32 +0530

    Update mars1.cpp

--- MARS TASK/mars1.cpp
+++ MARS TASK/mars1.cpp
@@ -1,27 +1,32 @@
+//mars task - medium 1 c++
+//ec23b1038 caitlin 
+
+
+
 #include <iostream>
 #include <functional>
 
-
-struct CustomData {
-    int value1;
-    double value2;
+//custom data
+struct cd {
+    int v1;
+    double v2;
 };
 
 int main() {
    
-    std::function<CustomData()> getData = []() -> CustomData {
-        CustomData data;
-        data.value1 = 10;
-        data.value2 = 3.14;
+    std::function<cd()> getData = []() -> cd {
+        cd data;
+        data.v1 = 10;
+        data.v2 = 3.14;
         return data;
     };
 
-   
-    CustomData retrievedData = getData();
+   //retrieved data
+    cd rd = getData();
 
-    // Display the retrieved custom data
-    std::cout << "Value 1: " << retrievedData.value1 << std::endl;
-    std::cout << "Value 2: " << retrievedData.value2 << std::endl;
+ 
+    std::cout << "Value 1: " << rd.v1 << std::endl;
+    std::cout << "Value 2: " << rd.v2 << std::endl;
 
     return 0;
 }
