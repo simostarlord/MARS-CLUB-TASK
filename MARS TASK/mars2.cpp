28c14a2f948a4868d2eea2e9ab9376dfe55da2fa
simostarlord <157518754+simostarlord@users.noreply.github.com>
2024-04-04 19:11:23 +0530

    Update mars2.cpp

--- MARS TASK/mars2.cpp
+++ MARS TASK/mars2.cpp
@@ -1,71 +1,69 @@
-//mars task - medium 2 c++
-//ec23b1038 caitlin 
-
-
-
+// ec23b1038 caitlin - mars medium task 2
 #include <iostream>
 #include <vector>
 #include <string>
 
 class Person {
 private:
-    std::string name;
-    int powerLevel;
+    std::string n;
+    int pl;
 
 public:
-    Person(const std::string& name, int powerLevel) : name(name), powerLevel(powerLevel) {}
+    Person(const std::string& name, int powerLevel) : n(name), pl(powerLevel) {}
 
     std::string getName() const {
-        return name;
+        return n;
     }
 
     int getPowerLevel() const {
-        return powerLevel;
+        return pl;
     }
 
     void displayInfo() const {
-        std::cout << "Name: " << name << ", Power Level: " << powerLevel << std::endl;
+        std::cout << "Name: " << n << ", Power Level: " << pl << std::endl;
     }
 };
 
-class mars {
+class Mars {
 private:
-    std::vector<Person> members;
+    std::vector<Person> mem;
 
 public:
-    void addMember(const Person& person) {
-        members.push_back(person);
+    void add(const Person& p) {
+        mem.push_back(p);
     }
 
-    void displayMembers() const {
-        std::cout << "mars Club Members:\n";
-        for (const auto& member : members) {
-            member.displayInfo();
+    void disp() const {
+        std::cout << "Mars Club Members:\n";
+        for (const auto& m : mem) {
+            m.displayInfo();
         }
     }
 
-    void Comm() const {
-        std::cout << "Intra Communication within mars Club Members is allowed.\n";
+    void com() const {
+        std::cout << "Intra Communication within Mars Club Members is allowed.\n";
     }
 };
 
 int main() {
-    mars mc;
+    Mars mc;
 
     // Adding members
     Person p1("rahul", 100);
     Person p2("caitlin", 80);
     Person p3("varsha", 120);
 
-    mc.addMember(p1);
-    mc.addMember(p2);
-    mc.addMember(p3);
+    mc.add(p1);
+    mc.add(p2);
+    mc.add(p3);
 
     // Displaying members
-    mc.displayMembers();
+    mc.disp();
 
     // Intra communication
-    mc.Comm();
+    mc.com();
 
     return 0;
 }
+
+
